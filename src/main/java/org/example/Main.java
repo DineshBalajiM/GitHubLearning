00a98a77eb586f4e82a5e717a7bff0edf1d4ff75
dinesh <dinesh.selvamala@gmail.com>
2025-11-15 16:48:02 +0530

    DineshBranch revoke commit123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         System.out.println("Conflicts Resolved");
 
-        System.out.println("DineshBranch revoke commit");
+        System.out.println("DineshBranch revoke commit123");
 
     }
 }
