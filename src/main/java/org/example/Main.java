cff2386383883be8ccc02f1c5c994cd561ba52ed
dinesh <dinesh.selvamala@gmail.com>
2025-11-15 16:49:03 +0530

    DineshBranch revoke commit latest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         System.out.println("Conflicts Resolved");
 
-        System.out.println("DineshBranch revoke commit123");
+        System.out.println("DineshBranch revoke commit latest");
 
     }
 }
