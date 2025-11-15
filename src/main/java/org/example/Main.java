34ba46901b2e821be70294231d0070b6bfd69f12
dinesh <dinesh.selvamala@gmail.com>
2025-11-15 16:30:19 +0530

    rebase commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,6 @@ public class Main {
         //Code from Dinesh Branch
         System.out.println("This is third commit from Dinesh's branch");
 
-        System.out.println("I am Dinesh Branch");
+        System.out.println("Resolved merge conflict");
     }
 }
