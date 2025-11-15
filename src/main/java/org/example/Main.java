a40bd0e691f312f45d881fdbce78a1618c7e0a20
dinesh <dinesh.selvamala@gmail.com>
2025-11-15 16:17:23 +0530

    bathri branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,5 +12,7 @@ public class Main {
 
         //Code from Dinesh Branch
         System.out.println("This is third commit from Dinesh's branch");
+
+        System.out.println("I am from bathri branch");
     }
 }
